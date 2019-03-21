ce0c25a056e6253094b0df71f3038ccf73c4182f
Victor Niño <ninomtz.victor@gmail.com>
2019-03-21 09:17:46 -0600

    Interfaz y controlador HorarioAlumno

---
+++ src/Interfaz/FXMLHorarioAlumnoController.java
@@ -0,0 +1,31 @@
+/**
+ * Institución: Universidad Veracruzana
+ * Programa educativo: Ingenieria de Software
+ * Descripción: Clase que contiene el controlador de la interfaz Horario 
+ * Alumno
+ * Modificación: 2019/03/21
+ * FXML Controller class
+ * @author Victor Niño
+ * @version 1.0
+ * @since 2019/03/21
+ */
+package Interfaz;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLHorarioAlumnoController implements Initializable {
+    
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
